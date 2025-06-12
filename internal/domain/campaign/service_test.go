157e3181b6c543b7b058aa95316301268e9b5aa5
alexduzi <duzihd@gmail.com>
2025-06-12 20:27:58 -0300

    isolating error handling

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -2,6 +2,7 @@ package campaign
 
 import (
 	"campaignemailsender/internal/contract"
+	internalerrors "campaignemailsender/internal/internal-errors"
 	"errors"
 	"testing"
 
@@ -87,5 +88,5 @@ func Test_Create_ValidateDataBaseSave(t *testing.T) {
 
 	_, err := service.Create(newCampaign)
 
-	assert.Equal("error to save on database", err.Error())
+	assert.True(errors.Is(internalerrors.ErrInternal, err))
 }
