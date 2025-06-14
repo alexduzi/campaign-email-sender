ad3ac9aede207c5edb974774810b1a0ee70b32ba
alexduzi <duzihd@gmail.com>
2025-06-14 14:02:18 -0300

    rest.http file and post http endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/go-chi/render"
 )
 
+type product struct {
+	ID   int
+	Name string
+}
+
 func main() {
 	r := chi.NewRouter()
 
@@ -32,6 +37,16 @@ func main() {
 		obj := map[string]string{"message": "success!"}
 
 		render.JSON(w, r, obj)
+	})
+
+	r.Post("/product", func(w http.ResponseWriter, r *http.Request) {
+		var product product
+
+		render.DecodeJSON(r.Body, &product)
+
+		product.ID = 5
+
+		render.JSON(w, r, product)
 	})
 
 	http.ListenAndServe(":3000", r)
