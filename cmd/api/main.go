dd58082849973a352fd4ba910178aed4adcfe123
alexduzi <duzihd@gmail.com>
2025-06-14 15:35:26 -0300

    campaign post

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"campaignemailsender/internal/contract"
+	"campaignemailsender/internal/domain/campaign"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -8,70 +10,33 @@ import (
 	"github.com/go-chi/render"
 )
 
-type product struct {
-	ID   int
-	Name string
-}
-
-type myHandler struct{}
-
-func (m myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("myHandler"))
-}
-
-func myMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		println("before")
-		next.ServeHTTP(w, r)
-		println("after")
-	})
-}
-
 func main() {
 	r := chi.NewRouter()
-
-	// m := myHandler{}
-	// r.Handle("/handler", m)
-
-	r.Use(myMiddleware)
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		product := r.URL.Query().Get("product")
-		id := r.URL.Query().Get("id")
+	service := campaign.Service{}
 
-		if product != "" && id != "" {
-			w.Write([]byte("Hello world -> " + product + " id: " + id))
-		} else {
-			w.Write([]byte("Hello world"))
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaign
+		err := render.DecodeJSON(r.Body, &request)
+		if err != nil {
+			println(err)
 		}
-	})
 
-	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
-		param := chi.URLParam(r, "productName")
+		id, err := service.Create(request)
 
-		w.Write([]byte("Hello world -> " + param))
-	})
+		if err != nil {
+			render.Status(r, 400)
+			render.JSON(w, r, map[string]string{"error": err.Error()})
+			return
+		}
 
-	r.Get("/json", func(w http.ResponseWriter, r *http.Request) {
-
-		obj := map[string]string{"message": "success!"}
-
-		render.JSON(w, r, obj)
-	})
-
-	r.Post("/product", func(w http.ResponseWriter, r *http.Request) {
-		var product product
-
-		render.DecodeJSON(r.Body, &product)
-
-		product.ID = 5
-
-		render.JSON(w, r, product)
+		render.Status(r, 201)
+		render.JSON(w, r, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":3000", r)
