36173d9fb73ba3628dd6a4a786d3726c5886a2c2
alexduzi <duzihd@gmail.com>
2025-06-14 17:50:30 -0300

    refactorings endpoints and new get endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"campaignemailsender/internal/contract"
 	"campaignemailsender/internal/domain/campaign"
+	"campaignemailsender/internal/endpoints"
 	"campaignemailsender/internal/infrastructure/database"
-	internalerrors "campaignemailsender/internal/internal-errors"
-	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/render"
 )
 
 func main() {
@@ -25,29 +22,10 @@ func main() {
 		Repository: &database.CampaignRepository{},
 	}
 
-	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
-		var request contract.NewCampaign
-		err := render.DecodeJSON(r.Body, &request)
-		if err != nil {
-			println(err)
-		}
+	handler := endpoints.Handler{CampaignService: service}
 
-		id, err := service.Create(request)
-
-		if err != nil {
-			if errors.Is(err, internalerrors.ErrInternal) {
-				render.Status(r, 500)
-
-			} else {
-				render.Status(r, 400)
-			}
-			render.JSON(w, r, map[string]string{"error": err.Error()})
-			return
-		}
-
-		render.Status(r, 201)
-		render.JSON(w, r, map[string]string{"id": id})
-	})
+	r.Post("/campaigns", handler.CampaignPost)
+	r.Get("/campaigns", handler.CampaignGet)
 
 	http.ListenAndServe(":3000", r)
 }
