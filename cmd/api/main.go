2d8eccf02c34035ba193c7be5716b1d660a3ce16
alexduzi <duzihd@gmail.com>
2025-06-13 21:15:05 -0300

    testing route params and query string params

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,7 +10,20 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world!"))
+		product := r.URL.Query().Get("product")
+		id := r.URL.Query().Get("id")
+
+		if product != "" && id != "" {
+			w.Write([]byte("Hello world -> " + product + " id: " + id))
+		} else {
+			w.Write([]byte("Hello world"))
+		}
+	})
+
+	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "productName")
+
+		w.Write([]byte("Hello world -> " + param))
 	})
 
 	http.ListenAndServe(":3000", r)
