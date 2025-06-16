4cc02e7fd081d08f4b576a913cd6014cc82874d6
alexduzi <duzihd@gmail.com>
2025-06-15 21:45:37 -0300

    adding new property status in campaign

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -90,3 +90,11 @@ func Test_NewCampaign_MustValidateEmail(t *testing.T) {
 
 	assert.Equal("email is invalid", err.Error())
 }
+
+func Test_NewCampaign_MustStatusStartWithPending(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Equal(Pending, campaign.Status)
+}
