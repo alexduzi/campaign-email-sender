457fe259e78c03c89b6d654a96cb4549ebe08580
alexduzi <duzihd@gmail.com>
2025-06-20 18:14:11 -0300

    decoding jwt to get email and set it into the context

--- internal/endpoints/auth.go
+++ internal/endpoints/auth.go
@@ -1,25 +1,27 @@
 package endpoints
 
 import (
+	"context"
 	"net/http"
 	"strings"
 
 	oidc "github.com/coreos/go-oidc/v3/oidc"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/render"
 )
 
 func Auth(nex http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		token := r.Header.Get("Authorization")
+		tokenString := r.Header.Get("Authorization")
 
-		if token == "" {
+		if tokenString == "" {
 			render.Status(r, http.StatusUnauthorized)
 			render.JSON(w, r, map[string]string{"error": "request does not contain an authorization header"})
 			return
 		}
 
-		token = strings.Replace(token, "Bearer ", "", 1)
+		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
 		provider, err := oidc.NewProvider(r.Context(), "http://localhost:8080/realms/provider")
 		if err != nil {
 			render.Status(r, http.StatusInternalServerError)
@@ -29,13 +31,19 @@ func Auth(nex http.Handler) http.Handler {
 
 		verifier := provider.Verifier(&oidc.Config{ClientID: "emailn"})
 		// verifier := provider.Verifier(&oidc.Config{SkipClientIDCheck: true})
-		_, err = verifier.Verify(r.Context(), token)
+		_, err = verifier.Verify(r.Context(), tokenString)
 		if err != nil {
 			render.Status(r, http.StatusUnauthorized)
 			render.JSON(w, r, map[string]string{"error": "invalid token"})
 			return
 		}
 
-		nex.ServeHTTP(w, r)
+		token, _ := jwt.Parse(tokenString, nil)
+		claims := token.Claims.(jwt.MapClaims)
+		email := claims["email"]
+
+		ctx := context.WithValue(r.Context(), "email", email)
+
+		nex.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
